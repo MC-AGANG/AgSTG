7f1c8635eb26a972cd1e64bc00afe9dcf8645e15
AGANG <54347161+MC-AGANG@users.noreply.github.com>
2026-02-07 19:50:38 +0800

    统一化GamePage

---
+++ AgSTG.Controls/GamePage.vb
@@ -0,0 +1,28 @@
+﻿Imports AgSTG
+Imports AgSTG.Core
+Public Class GamePage
+    Inherits Page
+    Public STG As New STG
+    Public PropertyBoard As New PropertyBoard
+    Public Sub SetBackground(fill As Brush)
+        CV_Main.Background = fill
+    End Sub
+    Public Sub New()
+        MyBase.New()
+        STG.Reset()
+    End Sub
+    Public Overrides Sub Initialize()
+        MyBase.Initialize()
+        Canvas.SetLeft(STG, 32)
+        Canvas.SetTop(STG, 16)
+        CV_Main.Children.Add(STG)
+        Canvas.SetLeft(PropertyBoard, 416)
+        Canvas.SetTop(PropertyBoard, 16)
+        CV_Main.Children.Add(PropertyBoard)
+    End Sub
+    Public Overrides Sub Act()
+        MyBase.Act()
+        STG.Render()
+        PropertyBoard.Update()
+    End Sub
+End Class
--- AgSTG.Controls/Page.xaml.vb
+++ AgSTG.Controls/Page.xaml.vb
@@ -1,4 +1,6 @@
-﻿Imports AgSTG.Core
+﻿Imports System.Collections.ObjectModel
+Imports System.Collections.Specialized
+Imports AgSTG.Core
 Public Class Page
     Public Timer As MediaTimer
     Public Property Activated As Boolean
@@ -16,25 +18,37 @@ Public Class Page
     End Property
     Private _Activated As Boolean
     Public Ticks As Long = 0
-    Public Act As Action = Nothing
-    Public Children As New List(Of Control)
+    Public WithEvents Controls As New ObservableCollection(Of Control)
+    Public Overridable Sub Initialize()
+
+    End Sub
+    Public Overridable Sub Act()
+
+    End Sub
     Public Sub Render()
         Ticks += 1
         Dispatcher.Invoke(Sub()
-                              If Not IsNothing(Act) Then
-                                  Act()
-                              End If
-                              For Each c In Children
+                              Act()
+
+                              For Each c In Controls
                                   c.Render()
                               Next
                           End Sub)
     End Sub
-    Public Sub Add(obj As Control)
-        Children.Add(obj)
-        CV_Main.Children.Add(obj)
+    Private Sub Update(sender As Object, e As NotifyCollectionChangedEventArgs)
+        If e.Action = NotifyCollectionChangedAction.Add Then
+            For Each ctl As Button In e.NewItems
+                CV_Main.Children.Add(ctl)
+            Next
+        ElseIf e.Action = NotifyCollectionChangedAction.Remove Then
+            For Each ctl As Button In e.OldItems
+                CV_Main.Children.Remove(ctl)
+            Next
+        End If
     End Sub
-    Public Sub Remove(obj As Control)
-        Children.Remove(obj)
-        CV_Main.Children.Remove(obj)
+
+    Private Sub UserControl_Loaded(sender As Object, e As RoutedEventArgs)
+        AddHandler Controls.CollectionChanged, AddressOf Update
+        Initialize()
     End Sub
 End Class
